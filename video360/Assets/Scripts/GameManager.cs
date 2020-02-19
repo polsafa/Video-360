b8ae2b87919cc184dab27689769a34e331e0c78e
polsafa <nork_pol@hotmail.es>
2020-02-19 14:15:10 +0100

    First commit

---
+++ video360/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//patron de programación singleton
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    private AudioClip[] arrayFX;
+    private AudioSource audioSource;
+    private bool initialez = false;
+
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject newManager = new GameObject("Game Manager");
+                _instance = newManager.AddComponent<GameManager>();
+                            newManager.AddComponent<AudioSource>();
+                DontDestroyOnLoad(_instance);
+            }
+
+            return _instance;
+        }
+    }
+
+    public bool initialized  { get{ return initialez; }  }
+
+    public void init()
+    {
+        initialez = true;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+        arrayFX = Resources.LoadAll<AudioClip>("Audios/FX");
+    }
+}
---
+++ video360/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GameManager.instance.initialized)
+            GameManager.instance.init();
+        
+    }
+
+    
+}
